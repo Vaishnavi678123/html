808b4a77f39ba737d6fb2287f6bb51e1f54298c4
Vaishnavi678123 <vaishnavi.kagita6@gmail.com>
2025-06-13 15:47:28 +0530

    Add files via upload

---
+++ components.js/app.js
@@ -0,0 +1,29 @@
+import './App.css';
+//import Intro from'./intro.js';
+import Rform from'./form.js';
+
+function App() {
+  return(
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={"https://www.sviet.edu.in/wp-content/uploads/2023/06/SVIET-LOGO-Edited-1.png"} className ="App-logo"alt="logo"/>
+          
+        <p>
+        Sri Vasavi institute of Engineering and technology
+        </p>
+        <a
+          className="App-link"
+          href="https://www.sviet.edu.in/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SVIET
+        </a>
+        <Intro/>
+      </header> */}
+      <Rform/>
+    </div>
+  );
+}
+
+export default App;
---
+++ components.js/intro.js
@@ -0,0 +1,9 @@
+function Intro()
+{
+    return(
+        <div>
+        <h1>Welcome to reactjs sesion</h1>
+        </div>
+    );    
+}
+export default Intro;
